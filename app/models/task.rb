43a26d170545987658b0cb3bf471e5178076c2b9
keidevelop <79072589+keidevelop@users.noreply.github>
2022-05-22 17:01:38 +0900

    モデル：taskに検証を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+
+  validate :validate_name_not_including_comma
+
+  private
+
+  def set_nameless_name
+    self.name = "無題" if name.blank?
+  end
+
+  #自作バリデーション削除してよい
+  def validate_name_not_including_comma
+    errors.add(:name, "にカンマを含めることはできません") if name&.include?(",")
+  end
 end
