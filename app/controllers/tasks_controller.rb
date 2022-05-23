5e4c3463b122ffd775fba7f1f482d8b0c82c1528
keidevelop <79072589+keidevelop@users.noreply.github>
2022-05-23 22:27:03 +0900

    タスクの昇順表示、scopeの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     #ログインユーザーのタスクのみ表示する
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.recent
   end
 
   def show
-    @task = current_user.tasks(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks(params[:id])
   end
 
   def create
@@ -26,7 +26,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = current_user.tasks(params[:id])
     if @task.update(task_params)
       redirect_to @task, notice: "タスク「#{@task.name}」を更新しました。"
     else
@@ -35,14 +34,17 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
